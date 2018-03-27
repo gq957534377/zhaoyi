5680653af27847599619db7123af14bc0ee84de6
guoqing <957534377@qq.com>
2018-03-27 21:45:56 +0800

    修改图标

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -7,13 +7,13 @@
     </li>
     <li class="tpl-left-nav-item">
         <a href="{{url('/log-viewer/logs')}}" class="nav-link @if(request()->path()=='log-viewer/logs') active @endif">
-            <i class="am-icon-home"></i>
+            <i class="am-icon-exclamation-triangle"></i>
             <span>日志管理</span>
         </a>
     </li>
     <li class="tpl-left-nav-item">
         <a href="javascript:;" class="nav-link tpl-left-nav-link-list">
-            <i class="am-icon-table"></i>
+            <i class="am-icon-female"></i>
             <span>角色管理</span>
             <i class="am-icon-angle-right tpl-left-nav-more-ico am-fr am-margin-right"></i>
         </a>
@@ -34,7 +34,7 @@
 
     <li class="tpl-left-nav-item">
         <a href="javascript:;" class="nav-link tpl-left-nav-link-list">
-            <i class="am-icon-table"></i>
+            <i class="am-icon-rocket"></i>
             <span>权限管理</span>
             <i class="am-icon-angle-right tpl-left-nav-more-ico am-fr am-margin-right"></i>
         </a>
@@ -55,7 +55,7 @@
 
     <li class="tpl-left-nav-item">
         <a href="javascript:;" class="nav-link tpl-left-nav-link-list">
-            <i class="am-icon-table"></i>
+            <i class="am-icon-user-secret"></i>
             <span>教务管理</span>
             <i class="am-icon-angle-right tpl-left-nav-more-ico am-fr am-margin-right"></i>
         </a>
@@ -76,7 +76,7 @@
 
     <li class="tpl-left-nav-item">
         <a href="javascript:;" class="nav-link tpl-left-nav-link-list">
-            <i class="am-icon-table"></i>
+            <i class="am-icon-graduation-cap"></i>
             <span>学生管理</span>
             <i class="am-icon-angle-right tpl-left-nav-more-ico am-fr am-margin-right"></i>
         </a>
@@ -97,7 +97,7 @@
 
     <li class="tpl-left-nav-item">
         <a href="javascript:;" class="nav-link tpl-left-nav-link-list">
-            <i class="am-icon-table"></i>
+            <i class="am-icon-user-plus"></i>
             <span>老师管理</span>
             <i class="am-icon-angle-right tpl-left-nav-more-ico am-fr am-margin-right"></i>
         </a>
@@ -118,7 +118,7 @@
 
     <li class="tpl-left-nav-item">
         <a href="javascript:;" class="nav-link tpl-left-nav-link-list">
-            <i class="am-icon-table"></i>
+            <i class="am-icon-book"></i>
             <span>课程管理</span>
             <i class="am-icon-angle-right tpl-left-nav-more-ico am-fr am-margin-right"></i>
         </a>
@@ -139,7 +139,7 @@
 
     <li class="tpl-left-nav-item">
         <a href="javascript:;" class="nav-link tpl-left-nav-link-list">
-            <i class="am-icon-table"></i>
+            <i class="am-icon-group"></i>
             <span>班级管理</span>
             <i class="am-icon-angle-right tpl-left-nav-more-ico am-fr am-margin-right"></i>
         </a>
@@ -181,26 +181,26 @@
 
     <li class="tpl-left-nav-item">
         <a href="{{url('/team_has_courses')}}" class="nav-link @if(request()->path()=='team_has_courses') active @endif">
-            <i class="am-icon-home"></i>
+            <i class="am-icon-th"></i>
             <span>班级课程管理</span>
         </a>
     </li>
     <li class="tpl-left-nav-item">
         <a href="{{url('/team_has_courses/my_course')}}" class="nav-link @if(request()->path()=='team_has_courses') active @endif">
-            <i class="am-icon-home"></i>
+            <i class="am-icon-th"></i>
             <span>我的课表</span>
         </a>
     </li>
     <li class="tpl-left-nav-item">
         <a href="{{url('/team_has_courses/my_team')}}" class="nav-link @if(request()->path()=='team_has_courses') active @endif">
-            <i class="am-icon-home"></i>
+            <i class="am-icon-book"></i>
             <span>我的课程</span>
         </a>
     </li>
 
     <li class="tpl-left-nav-item">
         <a href="javascript:;" class="nav-link tpl-left-nav-link-list">
-            <i class="am-icon-table"></i>
+            <i class="am-icon-comment-o"></i>
             <span>消息管理</span>
             <i class="am-icon-angle-right tpl-left-nav-more-ico am-fr am-margin-right"></i>
         </a>
